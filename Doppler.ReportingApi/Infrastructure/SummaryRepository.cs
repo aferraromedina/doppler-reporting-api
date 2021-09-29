d11d438690b51c9adfe78bc5030792f62fe76675
Adriano Ferraro <aferraro@makingsense.com>
2021-09-29 17:19:04 -0300

    fix: correct file formats according to rules

--- Doppler.ReportingApi/Infrastructure/SummaryRepository.cs
+++ Doppler.ReportingApi/Infrastructure/SummaryRepository.cs
@@ -21,31 +21,31 @@ namespace Doppler.ReportingApi.Infrastructure
             using (var connection = await _connectionFactory.GetConnection())
             {
                 var dummyDatabaseQuery = @"
-                SELECT 
-		                T.TotalSentEmails, 
-		                T.DistinctOpenedMailCount, 
-		                ISNULL(( T.DistinctOpenedMailCount / NULLIF (T.SendedMailCount,0 )), 0) ClickThroughRate
+                SELECT
+                        T.TotalSentEmails,
+                        T.DistinctOpenedMailCount,
+                        ISNULL(( T.DistinctOpenedMailCount / NULLIF (T.SendedMailCount,0 )), 0) ClickThroughRate
                 FROM (
-                    SELECT  
-	                    COUNT(*)  TotalSentEmails,    
+                    SELECT
+                        COUNT(*)  TotalSentEmails,
                         ISNULL(SUM(CASE IdDeliveryStatus WHEN 100 THEN 1 END),0) DistinctOpenedMailCount,
-	                    COUNT(CASE IdDeliveryStatus WHEN 0 THEN 1    
-                                                WHEN 1 THEN 1   
-                                                WHEN 3 THEN 1   
-                                                WHEN 4 THEN 1   
-                                                WHEN 5 THEN 1   
-                                                WHEN 6 THEN 1   
+                        COUNT(CASE IdDeliveryStatus WHEN 0 THEN 1
+                                                WHEN 1 THEN 1
+                                                WHEN 3 THEN 1
+                                                WHEN 4 THEN 1
+                                                WHEN 5 THEN 1
+                                                WHEN 6 THEN 1
                                                 WHEN 7 THEN 1   END)  SendedMailCount
 
                     FROM [user]
                         INNER JOIN Campaign WITH (NOLOCK) on [user].iduser = Campaign.IdUser
-	                    LEFT JOIN CampaignDeliveriesOpenInfo WITH(NOLOCK)  ON CampaignDeliveriesOpenInfo.IdCampaign = Campaign.IdCampaign
-                    WHERE 
-	                    Campaign.Status = 5 AND--SENT
-	                    Email = @userName AND
-	                    Campaign.UTCSentDate >= @startDate AND 
-	                    Campaign.UTCSentDate < @endDate
-	                ) T";
+                        LEFT JOIN CampaignDeliveriesOpenInfo WITH(NOLOCK)  ON CampaignDeliveriesOpenInfo.IdCampaign = Campaign.IdCampaign
+                    WHERE
+                        Campaign.Status = 5 AND--SENT
+                        Email = @userName AND
+                        Campaign.UTCSentDate >= @startDate AND
+                        Campaign.UTCSentDate < @endDate
+                    ) T";
 
                 var results = await connection.QueryAsync<CampaignsSummary>(dummyDatabaseQuery, new { userName, startDate, endDate });
                 var result = results.SingleOrDefault();
@@ -60,15 +60,15 @@ namespace Doppler.ReportingApi.Infrastructure
             using (var connection = await _connectionFactory.GetConnection())
             {
                 var dummyDatabaseQuery = @"
-                SELECT 
-	                (SELECT Count(1) FROM Subscriber S INNER JOIN [User] on [User].idUser = S.IdUser  WHERE [User].Email = @userName) AS TotalSubscribers,
-	                COUNT(1) as NewSubscribers,   
-	                COUNT(CASE WHEN  S.IdSubscribersStatus = 8 THEN 1 END) AS RemovedSubscribers 
-                FROM Subscriber S 
-	                INNER JOIN [User] on S.IdUser = [User].idUser
-	            WHERE [User].Email = @userName AND
-	  	            S.UTCCreationDate >= @startDate AND 
-		            S.UTCCreationDate < @endDate";
+                SELECT
+                    (SELECT Count(1) FROM Subscriber S INNER JOIN [User] on [User].idUser = S.IdUser  WHERE [User].Email = @userName) AS TotalSubscribers,
+                    COUNT(1) as NewSubscribers,
+                    COUNT(CASE WHEN  S.IdSubscribersStatus = 8 THEN 1 END) AS RemovedSubscribers
+                FROM Subscriber S
+                    INNER JOIN [User] on S.IdUser = [User].idUser
+                WHERE [User].Email = @userName AND
+                    S.UTCCreationDate >= @startDate AND
+                    S.UTCCreationDate < @endDate";
 
                 var results = await connection.QueryAsync<SubscribersSummary>(dummyDatabaseQuery, new { userName, startDate, endDate });
                 var result = results.SingleOrDefault();
